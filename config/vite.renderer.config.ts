dfcfd60588adb7d5814c5771df16f80dfa76970a
Yolium <65982085+yolium-ai@users.noreply.github.com>
2026-02-13 16:00:21 +0000

    (bug)Squash, Merge and PR button error (#240)

--- config/vite.renderer.config.ts
+++ config/vite.renderer.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
-  css: {
-    postcss: path.resolve(__dirname, 'postcss.config.js'),
-  },
+  plugins: [tailwindcss()],
   resolve: {
     alias: {
       '@renderer': path.resolve(__dirname, '../src/renderer'),
